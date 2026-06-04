eb829fd11b785f63ecc358f35f12998d0bbaaeaf
Brahim Lidrissi <blidriss@c3r9p13.1337.ma>
2026-06-04 21:37:17 +0100

    kinda done :)

--- src/client.c
+++ src/client.c
@@ -3,19 +3,43 @@
 #include "../ft_printf/ft_printf.h"
 
 
-void send_string(int pid, char *str)
+void send_bits(int pid, char c)
 {
+    int     i;
+    char    t;
+
+    i = 8;
+    t = 0;
+    while (--i >= 0)
+    {
+        t = c >> i;
+        if (t & 1)
+        {
+            kill(pid, SIGUSR1);
+        }else{
+            kill(pid, SIGUSR2);
+        }
+            usleep(555);
+    }
 
 }
 int main(int ac, char **av)
 {
     int     pid;
-
+    int     i;
+    int     j;
     pid = ft_atoi(av[1]);
-    if (pid <= 0 || !av[2])
+    if (pid <= 0)
         return 0;
-    send_string(pid, av[2]);
-
-
     
+    i = 1;
+    while (av[++i])
+    {    j = 0;
+        while (av[i][j])
+        {
+            send_bits(pid, av[i][j]);
+            j++;
+        }
+        send_bits(pid, ' ');
+    }
 }
--- src/server.c
+++ src/server.c
@@ -1,20 +1,86 @@
 #include <signal.h>
 #include "../ft_printf/ft_printf.h"
 
+unsigned char    byte;
+
+
 void handler(int sig)
 {
-    ft_printf("Signal is HEREEEEEEEEEEEE :(\n");
+    static int     bit;
+
+    if (sig == SIGUSR1)
+    {
+        byte |= (1 << 7 - bit);
+    }
+    bit++;
+    if (bit == 8)
+    {
+        ft_char(byte);
+        bit = 0;
+        byte = 0;
+    }
 }
 
 int main(void)
 {
     struct sigaction sa;
     sa.sa_handler = handler;
-    sigaction(11, &sa, NULL);
+
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
 
     ft_printf("PID: %d\n", getpid());
     while (1)
     {
-
+        pause();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void send_bits(int pid, char c)
+// {
+//     int     i;
+//     char    t;
+
+//     i = 8;
+//     t = 0;
+//     while (--i >= 0)
+//     {
+//         t = c >> i;
+//         if (t & 1)
+//             ft_printf("1 ");
+//         else
+//             ft_printf("0 ");
+//     }
+//     ft_printf("\n");
+// }
